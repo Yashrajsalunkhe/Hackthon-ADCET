ce657fd6fa5a86e31dfd8362529bf98de359e307
unknown <amityedage371@gmail.com>
2026-01-18 12:45:02 +0530

    Collage banner Design is changed

--- src/components/CollegeBanner.jsx
+++ src/components/CollegeBanner.jsx
@@ -5,7 +5,7 @@ const CollegeBanner = () => {
     <div className="college-banner">
       <div className="banner-container">
         <div className="banner-logo">
-          <img src="/img/logo.png" alt="ADCET Logo" className="logo-img" />
+          <img src="/img/adcet.png" alt="ADCET Logo" className="logo-img" />
         </div>
         
         <div className="banner-content">
@@ -13,21 +13,6 @@ const CollegeBanner = () => {
           <h2 className="banner-subtitle">
             Annasaheb Dange College of Engineering and Technology (ADCET), Ashta
           </h2>
-          <p className="banner-description">
-            An Empowered Autonomous Institute, affiliated to Shivaji University, Kolhapur
-          </p>
-          <p className="banner-approval">
-            Approved by AICTE, New Delhi & Govt. of Maharashtra
-          </p>
-          <div className="banner-accreditation">
-            <span className="accreditation-text">
-              Accredited by NAAC 'A+' Grade, Bangalore
-            </span>
-            <span className="accreditation-separator">•</span>
-            <span className="accreditation-highlight">
-              Eligible Programs Accredited by NBA, New Delhi
-            </span>
-          </div>
         </div>
       </div>
     </div>
